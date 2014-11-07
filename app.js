d10d99654acff443307cb479a1a6a0315a5b7bee
Silver Kuusik <silver.kuusik@gmail.com>
2014-11-07 19:35:36 +0100

    hardcoded PORT

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use(express.static(__dirname + "/public"));
 
 /* Landingpage */
 app.get('/', function(req, res) {
-	res.render('petricity');
+	res.render('index');
 });
 
 /* Start the express server */
-var server = app.listen(process.env.PORT, function() {
-	console.log("INFO", "express server listening on port:", process.env.PORT);
+var server = app.listen(3000, function() {
+	console.log("INFO", "express server listening on port:", 3000);
 });
 
 /* Start socket.io */
